a3ebd596759aad28239e9fc3b5f7d771a789e888
Christopher Wötzel <christopher.woetzel@cubus.eu>
2022-03-10 14:34:21 +0100

    add minimal working version of pactnet where dll is not copied

---
+++ PactDllCopyTest/Client.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PactDllCopyTest
+{
+    public class Client
+    {
+        private readonly HttpClient _client;
+
+        public Client(Uri baseUri)
+        {
+            _client = new HttpClient { BaseAddress = baseUri };
+        }
+
+        public async Task<IsOddObject> IsOdd(int number)
+        {
+            string formattedUrl = $"/api/IsOdd?value={number}";
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, formattedUrl);
+            request.Headers.Add("Accept", "application/json");
+
+            HttpResponseMessage response = await _client.SendAsync(request);
+
+            string content = await response.Content.ReadAsStringAsync();
+
+            request.Dispose();
+            response.Dispose();
+
+            return JsonConvert.DeserializeObject<IsOddObject>(content);
+        }
+    }
+
+    public struct IsOddObject
+    {
+        public bool isOdd;
+    }
+}
---
+++ PactDllCopyTest/Test.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using PactNet;
+using System.Net.Http;
+using System.Net;
+
+namespace PactDllCopyTest
+{
+    [TestFixture]
+    class Test
+    {
+        private IPactBuilderV3 pact;
+
+        [SetUp]
+        public void Setup()
+        {
+            pact = Pact.V3("consumer", "provider", new PactConfig()).UsingNativeBackend();
+        }
+        [Test]
+        public void IsOddTest()
+        {
+            pact
+                .UponReceiving("A Get Request for IsOdd")
+                    .WithRequest(HttpMethod.Get, "/api/IsOdd")
+                    .WithQuery("value", "99")
+                .WillRespond()
+                    .WithStatus(HttpStatusCode.OK)
+                    .WithHeader("Content-Type", "application/json")
+                    .WithJsonBody(new
+                    {
+                        isOdd = false
+                    });
+
+            pact.Verify(ctx =>
+            {
+                var client = new Client(ctx.MockServerUri);
+
+                IsOddObject result = client.IsOdd(99).Result;
+
+                Assert.AreEqual(false, result.isOdd);
+            });
+        } 
+    }
+}
